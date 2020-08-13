4570fac717f34730d3dedc2a341f98de85ef2e55
sam <samreality4@gmail.com>
2020-08-12 21:47:59 -0700

    added sign up component

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./sign-in.styles.scss";
 
 import FormInput from "../form-input/form-input.component";
-import CustomButton from "../custom-button/custom-buttom-component";
+import CustomButton from "../custom-button/custom-button-component";
 
 import { signInWithGoogle } from "../../firebase/firebase.utils";
 
