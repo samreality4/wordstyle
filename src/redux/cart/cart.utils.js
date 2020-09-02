1eb57cc57871ed36a6126795e3f0cec10deee6d6
sam <samreality4@gmail.com>
2020-09-01 17:11:55 -0700

    updated shop data with new data and updated cart util to group item together

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,15 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  //quantity property gets attached the first time around since this if block won't run when it's a new item
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
